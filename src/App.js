7b8fe8b0d83646e2428bedf008a62c51be6e3e4b
Test-repo <Realnafees@gmail.com>
2022-01-12 00:30:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Counter from "./Counter";
+import { Provider } from "react-redux";
+import store from "./store";
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
